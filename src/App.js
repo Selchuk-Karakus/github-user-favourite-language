4f83dfd5083a4d37b41b34d73fae95eb52ede69e
Selchuk Karakus <selchuk.karakus@gmail.com>
2022-08-10 16:56:54 +0100

    Add initial content and header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import './App.css';
 
+const Header = () => {
+  return (
+    <header>
+      <div  className="header-container">
+        <h1>Github User Favourite Language </h1>
+        <ul className="contacts">
+          <li className="contact-dev">
+            <a href="https://selchuk-karakus.vercel.app/">Get In Touch</a>
+          </li>
+          <li className="contact-mvf">
+            <a href="https://portal.mvfglobal.com/" title="">
+              MVF
+            </a>
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+}
+
 function App() {
   return (
     <div className="App">
-     <h1>SMOKE TEST</h1>
+     <Header/>
     </div>
   );
 }
